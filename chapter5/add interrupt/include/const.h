5a754bfcdf89bf6f3e50d22f35f46cfb9e54dd16
simowce <simowce@gmail.com>
2014-10-04 23:17:54 +0800

    Clean the folder "chapter5" which I didn't understand clearly now~

--- chapter5/add interrupt/include/const.h
+++ chapter5/add interrupt/include/const.h
@@ -5,5 +5,15 @@
 #define PRIVATE static
 
 #define GDT_SIZE 128
+#define IDT_SIZE 256
+
+#define INT_M_CTL 0x20
+#define INT_M_CTLMASK 0x21
+#define INT_S_CTL 0xA0
+#define INT_S_CTLMASK 0xA1
+
+#define PRIVILEGE_KRNL	0
+#define PRIVILEGE_TASK	1
+#define PRIVILEGE_USER	3
 
 #endif
